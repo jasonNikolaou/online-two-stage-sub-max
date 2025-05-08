866b38b2d508264b012b246137be0a533f78b09b
iasonas <nikolaou@bu.edu>
2025-05-08 17:45:55 -0400

    Update main and plots to repeat each experiment multiple times and report average and std

--- online-two-stage-sub-max-experiments/main.py
+++ online-two-stage-sub-max-experiments/main.py
@@ -8,172 +8,144 @@ import random
 random.seed(42)
 np.random.seed(42)
 
-settings = [{'dataset': 'wikipedia', 'T': 100, 'l': 20, 'k': 5, 'eta': 1, 'sample': True}]
-settings = [{'dataset': 'images', 'T': 250, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True}]
-settings = [{'dataset': 'teamformation', 'l': 10, 'k': 4, 'eta': 10, 'sample': False}]
+# settings = [{'dataset': 'wikipedia', 'T': 100, 'l': 20, 'k': 5, 'eta': 1, 'sample': True}]
+# settings = [{'dataset': 'images', 'T': 250, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True}]
+settings = [{'dataset': 'teamformation', 'l': 8, 'k': 4, 'eta': 10, 'sample': False}]
 # settings = [{'dataset': 'movies', 'T': 50, 'l': 10, 'k': 3, 'eta': 10, 'sample': True}]
-# settings = [{'dataset': 'influence', 'T': 100, 'l': 8, 'k': 3, 'eta': 10, 'sample': True}]
+# settings = [{'dataset': 'influence', 'T': 100, 'l': 8, 'k': 3, 'eta': 10, 'sample': False}]
+# settings = [{'dataset': 'coverage', 'T':100, 'l': 20, 'k': 1, 'eta': 0.1, 'sample': False}]
 
 
-# settings = [{'dataset': 'teamformation', 'l': 10, 'k': 4, 'eta': 0.1, 'sample': False}, 
-#             {'dataset': 'wikipedia', 'T': 100, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True},
-#             {'dataset': 'images', 'T': 200, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True}]
+# settings = [{'dataset': 'wikipedia', 'T': 100, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True},
+#             {'dataset': 'images', 'T': 200, 'l': 20, 'k': 5, 'eta': 0.1, 'sample': True},
+#             {'dataset': 'influence', 'T': 100, 'l': 8, 'k': 3, 'eta': 10, 'sample': True},
+#             {'dataset': 'movies', 'T': 50, 'l': 10, 'k': 3, 'eta': 10, 'sample': True},
+#             {'dataset': 'coverage', 'T':100, 'l': 20, 'k': 1, 'eta': 0.1, 'sample': False}]
+
 algorithms = ['FTRL-l2', 'FTRL-entropy', 'GA', 'one-stage GA', 'balkanski', 'repGreedy', 'OPT', 'Random']
-
-etas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+# algorithms = ['GA', 'one-stage GA', 'balkanski', 'repGreedy', 'OPT', 'Random']
+etas = [0.001, 0.01, 0.1, 1, 10]
+# etas = [0.01]
 
 for setting in settings:
     dataset = setting['dataset']
     print(f'=== Running experiments for dataset = {dataset} ===')
 
     with open(f'./instances/{dataset}.pkl', 'rb') as file:
-        print(f'dataset = {dataset}')
         wtps = pickle.load(file)
 
-    # Input
-    if setting['sample']:
-        T = setting['T']
-        fs = random.choices(wtps, k=T)
-    else:
-        fs = wtps
-        T = len(fs)
+    num_repeats = 5
+    all_results = []
+    best_etas = {}  # Store best eta per algorithm
 
-    n = len(wtps[0].potentials[0].w)
-    l = setting['l']
-    k = setting['k']
-    eta = setting['eta']
-
-    # Initial solution
-    x0 = np.zeros(n, dtype=int)
-    x0[:l] = 1
-    np.random.shuffle(x0)
-    results = dict()
-    if 'GA' in algorithms:
-        print(f'Running Gradient Ascent')
-        pipage = Pipage()
-        rewards = []
-        for eta in etas:
-            print(f'eta = {eta}')
-            ga = GradientAscent(l=l, k=k, n=n, eta=eta)
-            OL = OnlineLearning(fs, x0, k, ga, pipage)
-            OL.run()
-
-            if sum(OL.int_rewards) > sum(rewards):
-                rewards = OL.int_rewards
-                results['frac_rewards_GA'] = OL.frac_rewards
-                results['int_rewards_GA'] = OL.int_rewards
-                results['frac_sol_GA'] = OL.xs[-1]
-                results['int_sol_GA'] = OL.xs_int[-1]
-                results['eta_GA'] = eta
-
-    if 'one-stage GA' in algorithms:
-        print(f'Running one-stage Gradient Ascent')
-        pipage = Pipage()
-        rewards = []
-        for eta in etas:
-            print(f'eta = {eta}')
-            ga = GradientAscent(l=l, k=k, n=n, eta=eta, setting='one-stage')
-            OL = OnlineLearning(fs, x0, k, ga, pipage)
-            OL.run()
-            if sum(OL.int_rewards) > sum(rewards):
-                rewards = OL.int_rewards
-                results['one_stage_frac_rewards_GA'] = OL.frac_rewards
-                results['one_stage_int_rewards_GA'] = OL.int_rewards
-                results['frac_sol_one_stage_GA'] = OL.xs[-1]
-                results['int_sol_one_stage_GA'] = OL.xs_int[-1]
-                results['eta_one_stage_GA'] = eta
-
-    if 'FTRL-l2' in algorithms:
-        print(f'Running FTRL with L2 regularizer')
-        pipage = Pipage()
-        rewards = []
-        for eta in etas:
-            print(f'eta = {eta}')
-            ftrl = FTRL(l=l, k=k, n=n, eta=eta, regularizer='l2', setting='two-stage')
-            OL = OnlineLearning(fs, x0, k, ftrl, pipage)
-            OL.run()
-            
-            if sum(OL.int_rewards) > sum(rewards):
-                rewards = OL.int_rewards
-                results['frac_rewards_FTRL_l2'] = OL.frac_rewards
-                results['int_rewards_FTRL_l2'] = OL.int_rewards
-                results['frac_sol_FTRL_l2'] = OL.xs[-1]
-                results['int_sol_FTRL_l2'] = OL.xs_int[-1]
-                results['eta_FTRL_l2'] = eta
-
-    if 'FTRL-entropy' in algorithms:
-        print(f'Running FTRL with Entropic regularizer')
-        pipage = Pipage()
-        rewards = []
-        for eta in etas:
-            print(f'eta = {eta}')
-            ftrl = FTRL(l=l, k=k, n=n, eta=eta, regularizer='entropy', setting='two-stage')
-            OL = OnlineLearning(fs, x0, k, ftrl, pipage)
-            OL.run()
-
-            if sum(OL.int_rewards) > sum(rewards):
-                rewards = OL.int_rewards
-                results['frac_rewards_FTRL_entropy'] = OL.frac_rewards
-                results['int_rewards_FTRL_entropy'] = OL.int_rewards
-                results['frac_sol_FTRL_entropy'] = OL.xs[-1]
-                results['int_sol_FTRL_entropy'] = OL.xs_int[-1]
-                results['eta_FTRL_entropy'] = eta
-
-    if 'Random' in algorithms:
-        iterations = 10
-        print(f'Running random for {iterations} iterations')
-        rewards = np.zeros(len(fs))
-        for t in range(iterations):
-            randomAlg = Random(fs, n, l, k)
-            vals = randomAlg.solve()
-            rewards += np.array(vals)
-
-        rewards /= iterations
-        results['random'] = randomAlg.solve()
-
-
-    # === Offline algorithms ===
-    if 'OPT' in algorithms:
-        print(f'Calculating optimal solution in hindsight')
-        FracOpt = OptimalInHindsight(fs, l, k, 'fractional')
-        IntOpt = OptimalInHindsight(fs, l, k, 'integral')
-
-        fracSol, fracVal = FracOpt.solve()
-        intSol, intVal = IntOpt.solve()
-
-        results['frac_opt'] = fracVal
-        results['int_opt'] = intVal
-        results['frac_sol'] = fracSol
-        results['int_sol'] = intSol
-
-
-    if 'balkanski' in algorithms:
-        print(f"Balkasnki's rounding algorithm")
-        if not 'OPT' in algorithms:
-            print(f'Before running balkasnki algorithm, run OPT.')
+    for repeat in range(num_repeats):
+        print(f'--- Repeat {repeat+1}/{num_repeats} ---')
+        if setting['sample']:
+            T = 4 * len(wtps)
+            fs = random.choices(wtps, k=T)
         else:
-            balkanskiVal = 0
-            iterations = 10
-            print(f"Running Balkanski's algorithm {iterations} times.")
-            for t in range(iterations):
-                balkanski = Balkanski(fracSol, l, k, fs)
-                sol, val = balkanski.round()
-                balkanskiVal += val
-            balkanskiVal /= iterations
+            fs = wtps
+            T = len(fs)
 
-            results[f'balkanski_val'] = balkanskiVal
-            results['balkanski_sol'] = sol
+        n = len(wtps[0].potentials[0].w)
+        l = setting['l']
+        k = setting['k']
 
-    if 'repGreedy' in algorithms:
-        print(f'Running Replacement-Greedy algorithm')
-        repGreedy = ReplacementGreedy(fs, l, k, n)
-        repGreedySol, repGreedyVal = repGreedy.solve()
+        x0 = np.ones(n, dtype=int)
+        x0 = x0 * k / n
+        np.random.shuffle(x0)
 
-        results[f'repGreedy_val'] = repGreedyVal
-        results['repGreedy_sol'] = repGreedySol
+        results = dict()
+        pipage = Pipage()
 
-    with open(f'./results/{dataset}.pkl', 'wb') as file:
-        pickle.dump(results, file) 
+        def run_with_eta_selection(name, constructor):
+            rewards = []
+            if repeat == 0:
+                etas_ = etas
+                if dataset == 'movies':
+                    etas_ = [0.001, 0.01, 1]
+                elif dataset == 'wikipedia':
+                    etas_ = [0.1, 1]
+                elif dataset == 'influence':
+                    etas_ = [0.1]
+
+                for eta_val in etas_:
+                    print(f'{name}: eta = {eta_val}')
+                    learner = constructor(eta_val)
+                    OL = OnlineLearning(fs, x0, k, learner, pipage)
+                    OL.run()
+                    if sum(OL.frac_rewards) > sum(rewards):
+                        rewards[:] = OL.frac_rewards
+                        results[f'frac_rewards_{name}'] = OL.frac_rewards
+                        results[f'int_rewards_{name}'] = OL.int_rewards
+                        results[f'frac_sol_{name}'] = OL.xs
+                        results[f'int_sol_{name}'] = OL.xs_int
+                        results[f'eta_{name}'] = eta_val
+                        best_etas[name] = eta_val
+            else:
+                eta_val = best_etas[name]
+                print(f'{name}: using best eta = {eta_val}')
+                learner = constructor(eta_val)
+                OL = OnlineLearning(fs, x0, k, learner, pipage)
+                OL.run()
+                results[f'frac_rewards_{name}'] = OL.frac_rewards
+                results[f'int_rewards_{name}'] = OL.int_rewards
+                results[f'frac_sol_{name}'] = OL.xs
+                results[f'int_sol_{name}'] = OL.xs_int
+                results[f'eta_{name}'] = eta_val
+
+        if 'GA' in algorithms:
+            run_with_eta_selection('GA', lambda eta, *_: GradientAscent(l=l, k=k, n=n, eta=eta, setting='two-stage'))
+
+        if 'one-stage GA' in algorithms:
+            run_with_eta_selection('one_stage_GA', lambda eta, *_: GradientAscent(l=l, k=k, n=n, eta=eta, setting='one-stage'))
+
+        if 'FTRL-l2' in algorithms:
+            run_with_eta_selection('FTRL_l2', lambda eta, *_: FTRL(l=l, k=k, n=n, eta=eta, regularizer='l2', setting='two-stage'))
+
+        if 'FTRL-entropy' in algorithms:
+            run_with_eta_selection('FTRL_entropy', lambda eta, *_: FTRL(l=l, k=k, n=n, eta=eta, regularizer='entropy', setting='two-stage'))
+
+        if 'Random' in algorithms:
+            iterations = 5
+            print(f'Running Random for {iterations} iterations')
+            results['int_rewards_all_random'] = []
+            for _ in range(iterations):
+                alg = Random(fs, n, l, k)
+                rewards = alg.solve()
+                results['int_rewards_all_random'].append(rewards)
+
+        if 'OPT' in algorithms:
+            print(f'Calculating OPT')
+            FracOpt = OptimalInHindsight(fs, l, k, 'fractional')
+            IntOpt = OptimalInHindsight(fs, l, k, 'integral')
+            fracSol, fracVal = FracOpt.solve()
+            intSol, intVal = IntOpt.solve()
+            results['frac_opt'] = fracVal
+            results['int_opt'] = intVal
+            results['frac_sol'] = fracSol
+            results['int_sol'] = intSol
+
+        if 'balkanski' in algorithms and 'frac_sol' in results:
+            print(f'Running Balkanski')
+            vals = []
+            for _ in range(5):
+                balk = Balkanski(results['frac_sol'], l, k, fs)
+                _, val = balk.round()
+                vals.append(val)
+            results['int_rewards_all_balkanski'] = vals
+
+        if 'repGreedy' in algorithms:
+            print(f'Running ReplacementGreedy')
+            rep = ReplacementGreedy(fs, l, k, n)
+            sol, val = rep.solve()
+            results['int_rewards_repGreedy'] = val
+            results['repGreedy_sol'] = sol
+
+        all_results.append(results)
+
+    with open(f'./results/{dataset}.pkl', 'wb') as f:
+        pickle.dump(all_results, f)
 
     
 
--- online-two-stage-sub-max-experiments/plots.py
+++ online-two-stage-sub-max-experiments/plots.py
@@ -5,14 +5,16 @@ import pickle
 settings = [{'dataset': 'wikipedia', 'title': 'Wikipedia articles representatives'},
             {'dataset': 'images', 'title': 'Image summarization'},
             {'dataset': 'teamformation', 'title': 'Team formation'},
-            {'dataset': 'influence', 'title': 'Influence maximization'}]
+            {'dataset': 'influence', 'title': 'Influence maximization'},
+            {'dataset': 'movies', 'title': 'Movie recommendation'},
+            {'dataset': 'coverage', 'title': 'Coverage'}]
 
 # settings = [{'dataset': 'images', 'title': 'Image summarization'}]
 # settings = [{'dataset': 'wikipedia', 'title': 'Wikipedia articles representatives'}]
 settings = [{'dataset': 'teamformation', 'title': 'Team formation'}]
 # settings = [{'dataset': 'movies', 'title': 'Movie recommendation'}]
 # settings = [{'dataset': 'influence', 'title': 'Influence maximization'}]
-settings = [{'dataset': 'coverage', 'title': 'Coverage'}]
+# settings = [{'dataset': 'coverage', 'title': 'Coverage'}]
 
 
 
@@ -20,161 +22,165 @@ cmap = plt.get_cmap("viridis")  # colorblind palette
 marker_styles = ['o', 's', '^', 'D', 'v', 'P', '*', 'X']  # Unique markers for each line
 
 for setting in settings:
+    print(f"Dataset = {setting['dataset']}")
     dataset = setting['dataset']
     title = setting['title']
     with open(f'./results/{dataset}.pkl', 'rb') as file:
         data = pickle.load(file)
 
+    n_lines = 0
     # Retrieve the rewards array from the dictionary
-    if 'frac_rewards_GA' in data:
-        frac_rewards_GA = data["frac_rewards_GA"]
-        int_rewards_GA = data["int_rewards_GA"]
-        eta_GA = data['eta_GA']
-        print(f'eta GA = {eta_GA}')
-        print(f"GA sol = {np.round(data['frac_sol_GA'], 1)}")
-        T = len(frac_rewards_GA)
-    if 'frac_rewards_FTRL_l2' in data:
-        frac_rewards_FTRL_l2 = data["frac_rewards_FTRL_l2"]
-        int_rewards_FTRL_l2 = data["int_rewards_FTRL_l2"]
-        eta_FTRL_l2 = data['eta_FTRL_l2']
-        print(f'eta FTRL l2 = {eta_FTRL_l2}')
-        T = len(frac_rewards_FTRL_l2)
-    if 'frac_rewards_FTRL_entropy' in data:   
-        frac_rewards_FTRL_entropy = data["frac_rewards_FTRL_entropy"]
-        int_rewards_FTRL_entropy = data["int_rewards_FTRL_entropy"]
-        eta_FTRL_entropy = data['eta_FTRL_entropy']
-        print(f'eta FTRL entropy = {eta_FTRL_entropy}')
-        T = len(frac_rewards_FTRL_entropy)
-    if 'one_stage_frac_rewards_GA' in data:
-        frac_rewards_one_stage_GA = data['one_stage_frac_rewards_GA']
-        int_rewards_one_stage_GA = data['one_stage_int_rewards_GA']
-        eta_one_stage_GA = data['eta_one_stage_GA']
-        print(f"one stage GA sol = {np.round(data['frac_sol_one_stage_GA'], 1)}")
+    if 'frac_rewards_GA' in data[0]:
+        frac_rewards_GA = [res["frac_rewards_GA"] for res in data]
+        int_rewards_GA = [res["int_rewards_GA"] for res in data]
+        etas_GA = [res['eta_GA'] for res in data]
+        print(f'eta GA = {etas_GA}')
+        T = len(frac_rewards_GA[0])
+        n_lines += 1
 
-        print(f'eta one-stage GA = {eta_one_stage_GA}')
+    if 'frac_rewards_FTRL_l2' in data[0]:
+        frac_rewards_FTRL_l2 = [res["frac_rewards_FTRL_l2"] for res in data]
+        int_rewards_FTRL_l2 = [res["int_rewards_FTRL_l2"] for res in data]
+        etas_FTRL_l2 = [res['eta_FTRL_l2'] for res in data]
+        print(f'eta FTRL l2 = {etas_FTRL_l2}')
+        n_lines += 1
 
-    if 'random' in data:
-        rewards_random = data['random']
+    if 'frac_rewards_FTRL_entropy' in data[0]:   
+        frac_rewards_FTRL_entropy = [res["frac_rewards_FTRL_entropy"] for res in data]
+        int_rewards_FTRL_entropy = [res["int_rewards_FTRL_entropy"] for res in data]
+        etas_FTRL_entropy = [res['eta_FTRL_entropy'] for res in data]
+        print(f'eta FTRL entropy = {etas_FTRL_entropy}')
+        n_lines += 1
+        
+    if 'frac_rewards_one_stage_GA' in data[0]:
+        frac_rewards_one_stage_GA = [res['frac_rewards_one_stage_GA'] for res in data]
+        int_rewards_one_stage_GA = [res['int_rewards_one_stage_GA'] for res in data]
+        etas_one_stage_GA = [res['eta_one_stage_GA'] for res in data]
+        print(f'eta one-stage GA = {etas_one_stage_GA}')
+        n_lines += 1
 
-    if 'frac_opt' in data:
-        frac_opt = data['frac_opt']
-        int_opt = data['int_opt']
-        print(f"int_sol = {data['int_sol']}")
+    if 'int_rewards_all_random' in data[0]:
+        int_rewards_all_random = [res['int_rewards_all_random'] for res in data]
+        n_lines += 1
 
-    if 'balkanski_val' in data:
-        balkanskiVal = data[f'balkanski_val']
+    if 'frac_opt' in data[0]:
+        frac_opt = [res['frac_opt'] for res in data]
+        int_opt = [res['int_opt'] for res in data]
+        n_lines += 1
 
-    if 'repGreedy_val' in data:
-        repGreedyVal = data['repGreedy_val']
+    if 'int_rewards_all_balkanski' in data[0]:
+        int_rewards_all_balkanski = [res[f'int_rewards_all_balkanski'] for res in data]
+        n_lines += 1
 
-    # print(data['int_sol_GA'])
-    # print(data['int_sol'])
+    if 'int_rewards_repGreedy' in data[0]:
+        int_rewards_repGreedy = [res['int_rewards_repGreedy'] for res in data]
+        n_lines += 1
 
-    
     timesteps = np.arange(1, T+1)
 
-    # Compute cumulative sum of rewards and running average
-    if 'frac_rewards_GA' in data:
-        cum_frac_GA = np.cumsum(frac_rewards_GA)
-        cum_int_GA = np.cumsum(int_rewards_GA)
-        avg_cum_frac_GA = cum_frac_GA / timesteps
-        avg_cum_int_GA = cum_int_GA / timesteps
-    if 'frac_rewards_FTRL_l2' in data:
-        cum_frac_FTRL_l2 = np.cumsum(frac_rewards_FTRL_l2)
-        cum_int_FTRL_l2 = np.cumsum(int_rewards_FTRL_l2)
-        avg_cum_frac_FTRL_l2 = cum_frac_FTRL_l2 / timesteps
-        avg_cum_int_FTRL_l2 = cum_int_FTRL_l2 / timesteps
-    if 'frac_rewards_FTRL_entropy' in data:
-        cum_frac_FTRL_entropy = np.cumsum(frac_rewards_FTRL_entropy)
-        cum_int_FTRL_entropy = np.cumsum(int_rewards_FTRL_entropy)
-        avg_cum_frac_FTRL_entropy = cum_frac_FTRL_entropy / timesteps
-        avg_cum_int_FTRL_entropy = cum_int_FTRL_entropy / timesteps
-    if 'one_stage_frac_rewards_GA' in data:
-        cum_frac_one_stage_GA = np.cumsum(frac_rewards_one_stage_GA)
-        cum_int_one_stage_GA = np.cumsum(int_rewards_one_stage_GA)
-        avg_cum_frac_one_stage_GA = cum_frac_one_stage_GA / timesteps
-        avg_cum_int_one_stage_GA = cum_int_one_stage_GA / timesteps
-    if 'random' in data:
-        cum_random = np.cumsum(rewards_random)
-        avg_cum_random = cum_random / timesteps
+    def compute_avg_cum_and_std(rewards_all):
+        """
+        Args:
+            rewards_all:
+                - A 2D array of shape (N, T)
+                - OR a list of 2D arrays of shape (N_i, T)
+                - OR a list of 1D arrays of shape (T,) to be stacked into (N, T)
+        Returns:
+            avg_cum: array of shape (T,)
+            std_cum: array of shape (T,)
+        """
+        # If it's a list, validate elements and stack
+        if isinstance(rewards_all, list):
+            processed = []
+            for r in rewards_all:
+                r = np.array(r)
+                if r.ndim == 1:
+                    r = r.reshape(1, -1)  # shape (1, T)
+                elif r.ndim != 2:
+                    raise ValueError(f"List element has unexpected shape: {r.shape}")
+                processed.append(r)
+            rewards_all = np.concatenate(processed, axis=0)  # shape (N_total, T)
 
+        elif isinstance(rewards_all, np.ndarray):
+            if rewards_all.ndim == 1:
+                rewards_all = rewards_all.reshape(1, -1)
+            elif rewards_all.ndim != 2:
+                raise ValueError(f"Expected 2D array, got shape {rewards_all.shape}")
+        else:
+            raise TypeError("rewards_all must be a list or a numpy array")
 
-    # Plot the rewards
-    # plt.plot(avg_cum_int_GA, label='GA')
-    # plt.plot(avg_cum_int_one_stage_GA, label='one-stage GA')
-    # plt.plot(avg_cum_int_FTRL_l2, label='FTRL - L2')
-    # plt.plot(avg_cum_int_FTRL_entropy, label='FTRL - entropy')
-    # plt.plot([int_opt / T] * T, linestyle='--', linewidth=1.5, label='OPT')
-    # plt.plot([balkanskiVal / T] * T, linestyle='-.', linewidth=1.5, label='CONTINUOUS-OPT')
-    # plt.plot([repGreedyVal / T] * T, linestyle='-.', linewidth=1.5, label='REP-GREEDY')
+        # Now guaranteed to be (N_total, T)
+        cum_rewards = np.cumsum(rewards_all, axis=1)
+        T = rewards_all.shape[1]
+        timesteps = np.arange(1, T + 1).reshape(1, -1)
 
+        avg_cum = np.mean(cum_rewards / timesteps, axis=0)
+        std_cum = np.std(cum_rewards / timesteps, axis=0)
 
-    # Prepare data and labels
-    lines = []
-    if 'frac_rewards_GA' in data:
-        lines.append((avg_cum_int_GA, 'RAOCO-OGA'))
-    if 'frac_rewards_FTRL_l2' in data:
-        lines.append((avg_cum_int_FTRL_l2, 'RAOCO-FTRL-$L_2$'))
-    if 'frac_rewards_FTRL_entropy' in data:
-        lines.append((avg_cum_int_FTRL_entropy, 'RAOCO-FTRL-H'))
-    if 'one_stage_frac_rewards_GA' in data:
-        lines.append((avg_cum_int_one_stage_GA, '1S-OGA'))
-    if 'random' in data:
-        lines.append((avg_cum_random, 'Random'))
+        return avg_cum, std_cum
 
-    # lines = [
-    #     (avg_cum_int_GA, 'RAOCO-OGA'),
-    #     (avg_cum_int_FTRL_l2, 'RAOCO-FTRL-$L_2$'),
-    #     (avg_cum_int_FTRL_entropy, 'RAOCO-FTRL-H'),
-    #     (avg_cum_int_one_stage_GA, '1S-OGA'),
-    #     (avg_cum_random, 'Random')
-    # ]
-    fixed_lines = []
-    if 'frac_opt' in data:
-        fixed_lines.append(([int_opt / T] * T, '--', 'OFLN - OPT'))
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plot_idx = 0
 
-    if 'balkanski_val' in data:
-        fixed_lines.append(([balkanskiVal / T] * T, '-.', 'OFLN - CO'),)
+    if 'int_rewards_GA' in data[0]:
+        avg_cum_int_GA, std_cum_GA = compute_avg_cum_and_std(int_rewards_GA)
+        ax.plot(avg_cum_int_GA, label='RAOCO-OGA', color=cmap(plot_idx / (n_lines - 1)), marker=marker_styles[plot_idx], markevery=int(T / 10))
+        ax.fill_between(timesteps, avg_cum_int_GA - std_cum_GA, avg_cum_int_GA + std_cum_GA, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    if 'repGreedy_val' in data:
-        fixed_lines.append(([repGreedyVal / T] * T, '-.', 'OFLN - RGR'))
+    if 'int_rewards_FTRL_l2' in data[0]:
+        avg_cum_FTRL_l2, std_cum_FTRL_l2 = compute_avg_cum_and_std(int_rewards_FTRL_l2)
+        ax.plot(avg_cum_FTRL_l2, label='RAOCO-FTRL-$L_2$', color=cmap(plot_idx / (n_lines - 1)), marker=marker_styles[plot_idx], markevery=int(T / 10))
+        ax.fill_between(timesteps, avg_cum_FTRL_l2 - std_cum_FTRL_l2, avg_cum_FTRL_l2 + std_cum_FTRL_l2, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    # fixed_lines = [
-    #     ([int_opt / T] * T, '--', 'OFLN - OPT'),
-    #     ([balkanskiVal / T] * T, '-.', 'OFLN - CO'),
-    #     ([repGreedyVal / T] * T, '-.', 'OFLN - RGR'),
-    # ]
+    if 'int_rewards_FTRL_entropy' in data[0]:
+        avg_cum_FTRL_entropy, std_cum_FTRL_entropy = compute_avg_cum_and_std(int_rewards_FTRL_entropy)
+        ax.plot(avg_cum_FTRL_entropy, label='RAOCO-FTRL-H', color=cmap(plot_idx / (n_lines - 1)), marker=marker_styles[plot_idx], markevery=int(T / 10))
+        ax.fill_between(timesteps, avg_cum_FTRL_entropy - std_cum_FTRL_entropy, avg_cum_FTRL_entropy + std_cum_FTRL_entropy, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    num_lines = len(lines) + len(fixed_lines)
-    colors = [cmap(i / (num_lines - 1)) for i in range(num_lines)]
+    if 'int_rewards_one_stage_GA' in data[0]:
+        avg_cum_1S, std_cum_1S = compute_avg_cum_and_std(int_rewards_one_stage_GA)
+        ax.plot(avg_cum_1S, label='1S-OGA', color=cmap(plot_idx / (n_lines - 1)), marker=marker_styles[plot_idx], markevery=int(T / 10))
+        ax.fill_between(timesteps, avg_cum_1S - std_cum_1S, avg_cum_1S + std_cum_1S, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    # Plot lines with markers
-    for i, (data, label) in enumerate(lines):
-        plt.plot(
-            data,
-            label=label,
-            color=colors[i],
-            marker=marker_styles[i],
-            markevery=10,  # Place markers every 10 points
-        )
+    if 'int_rewards_all_random' in data[0]:
+        avg_cum_rand, std_cum_rand = compute_avg_cum_and_std(int_rewards_all_random)
+        ax.plot(avg_cum_rand, label='Random', color=cmap(plot_idx / (n_lines - 1)), marker=marker_styles[plot_idx], markevery=int(T / 10))
+        ax.fill_between(timesteps, avg_cum_rand - std_cum_rand, avg_cum_rand + std_cum_rand, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    # Plot fixed lines (optional)
-    for i, (data, style, label) in enumerate(fixed_lines):
-        plt.plot(
-            data,
-            linestyle=style,
-            linewidth=1.5,
-            label=label,
-            color=colors[len(lines) + i],
-            marker=marker_styles[len(lines) + i],
-            markevery=10,  
-        )
+    if 'int_rewards_all_balkanski' in data[0]:
+        int_rewards_all_balkanski = [[val/T] * T for run in int_rewards_all_balkanski for val in run]
+        avg_cum_balk, std_cum_balk = compute_avg_cum_and_std(int_rewards_all_balkanski)
+        ax.plot(avg_cum_balk, label='OFLN - CO', color=cmap(plot_idx / (n_lines - 1)), linestyle='-.', linewidth=1.5,
+                marker=marker_styles[plot_idx], markevery=int(T/10))
+        ax.fill_between(timesteps, avg_cum_balk - std_cum_balk, avg_cum_balk + std_cum_balk, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    # plt.title(title)
-    plt.xlabel("Time Steps", fontsize=16)
-    plt.ylabel("$C_t$", fontsize=16)
-    plt.legend()
-    plt.grid(True)
+    # Fixed baseline
+    if 'int_opt' in data[0]:
+        int_rewards_opt = [[val/T] * T for val in int_opt]
+        avg_cum_opt, std_cum_opt = compute_avg_cum_and_std(int_rewards_opt)
+        ax.plot(avg_cum_opt, label='OFLN - OPT', color=cmap(plot_idx / (n_lines - 1)), linestyle='-.', linewidth=1.5,
+                marker=marker_styles[plot_idx], markevery=int(T/10))
+        ax.fill_between(timesteps, avg_cum_opt - std_cum_opt, avg_cum_opt + std_cum_opt, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
 
-    plt.savefig(f'./plots/{dataset}.pdf', bbox_inches='tight', )
-    plt.close()  # Close the plot to avoid display
+    if 'int_rewards_repGreedy' in data[0]:
+        int_rewards_repGreedy = [[val/T] * T for val in int_rewards_repGreedy]
+        avg_cum_repGreedy, std_cum_repGreedy = compute_avg_cum_and_std(int_rewards_repGreedy)
+        ax.plot(avg_cum_repGreedy, label='OFLN - RGR', color=cmap(plot_idx / (n_lines - 1)), linestyle='-.', linewidth=1.5,
+                marker=marker_styles[plot_idx], markevery=int(T/10))
+        ax.fill_between(timesteps, avg_cum_repGreedy - std_cum_repGreedy, avg_cum_repGreedy + std_cum_repGreedy, color=cmap(plot_idx / (n_lines - 1)), alpha=0.2)
+        plot_idx += 1
+
+    ax.set_xlabel("Time Steps", fontsize=16)
+    ax.set_ylabel("$C_t$", fontsize=16)
+    ax.legend(loc='lower left')
+    # ax.legend()
+    ax.grid(True)
+    fig.savefig(f'./plots/{dataset}.pdf', bbox_inches='tight')
+    plt.close(fig)
